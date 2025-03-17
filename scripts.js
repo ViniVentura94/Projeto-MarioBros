a40b331c16f56f7557db13a5f1fa655e7d9745ce
ViniVentura94 <viniventura94@gmail.com>
2025-03-17 16:10:30 -0300

    Criado repositório

---
+++ scripts.js
@@ -0,0 +1,17 @@
+
+const form = document.querySelector('.formulario');
+const mascara = document.querySelector('.mascara-formulario');
+
+
+function cliqueiNoBotao() {
+    form.style.left = "50%";
+    form.style.transform = "translateX(-50%)";
+    form.style.zIndex = "1";
+    mascara.style.visibility = "visible";
+}
+
+function cliqueiNaMascara() {
+    mascara.style.visibility = "hidden";
+    form.style.transform = "translateX(0)"
+    form.style.left = "-100%";
+}
